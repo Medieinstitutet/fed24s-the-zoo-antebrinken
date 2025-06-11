885554e0cc6166de2146c3bca8b276ed805493dc
Philip Antebrink <antebrinken@live.se>
2025-06-11 11:11:52 +0200

    Debugging and trying to solve issues

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,23 +1,26 @@
-import { Outlet, Link} from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+
 
 export default function MainLayout() {
-    return (
-        <div>
-            <header style={{ padding: "1rem", backgroundColor: "#ddd" }}>
-        <nav>
-          <Link to="/" style={{ marginRight: "1rem" }}>Hem</Link>
-          <Link to="/animals">Djur</Link>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-black p-6">
+        <div className="flex justify-between items-center max-w-7xl mx-auto">
+          <h1 className="text-white text-xl font-semibold"> The Zoo App by Philip</h1>
+        <nav className="flex space-x-8">
+          <Link to="/" className="mr-4 text-blue-700 hover:underline">Hem</Link>
+          <Link to="/animals" className="text-blue-700 hover:underline">Djur</Link>
         </nav>
+        </div>
       </header>
 
-      <main style={{ padding: "1rem"}}>
-        <Outlet /> {}
+      <main className="p-4 flex-grow">
+        <Outlet />
       </main>
 
-       <footer style={{ padding: "1rem", backgroundColor: "#ddd", marginTop: "2rem" }}>
-        &copy; 2025 Zoo App
-      </footer>
+      <footer className="w-full h-[50px] bg-black text-white mt-8 text-center flex items-center justify-center">
+  &copy; 2025 Zoo App
+</footer>
     </div>
-
-    )
+  );
 }
